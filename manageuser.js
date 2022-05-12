87358ef0f0da73a7628ecefcd2b1165328445938
Spencer Ehardt <spencerehardt@gmail.com>
2022-05-12 15:44:23 -0500

    Changed to send text

--- manageuser.js
+++ manageuser.js
@@ -2,8 +2,13 @@
 
 let userName = "";
 let password = "";
+let phonenumber = "";
 let verifypassword = "";
 let passwordRegEx=/((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%!]).{6,40})/;
+
+function setphonenumber(){
+    phonenumber = $("#phonenumber").val();
+}
 
 function setusername(){
     userName = $("#username").val();
@@ -46,9 +51,20 @@ function checkexpiredtoken(token){
     }
 }
 
+function sendtext(){
+    setphonenumber();
+
+    $.ajax({
+        type: 'POST',
+        url: 'https://dev.stedi.me/twofactorlogin/' + phonenumber,
+        contentType: 'application/text',
+        dataType: 'text'
+    })
+}
+
 function userlogin(){
     setuserpassword();
-    setusername();
+
     $.ajax({
         type: 'POST',
         url: 'https://dev.stedi.me/login',
